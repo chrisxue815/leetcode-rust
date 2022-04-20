cd2ca9927b69245904a98260ede08e60fbc35741
Chris Xue <chrisxue815@gmail.com>
2022-04-20 03:01:31 -0400

    227. Basic Calculator II

--- src/test_0227.rs
+++ src/test_0227.rs
@@ -7,7 +7,28 @@ impl Solution {
         let len = s.len();
         let mut sum = 0;
         let mut sign = 1;
-        let (mut product, mut i) = read_number(s, 0);
+
+        let read_number = |mut i| -> (usize, i32) {
+            while i < len && s[i] == b' ' {
+                i += 1;
+            }
+
+            let mut num: i32 = 0;
+
+            while i < len {
+                let c = s[i];
+                if c < b'0' || c > b'9' {
+                    break;
+                }
+                num *= 10;
+                num += (s[i] - b'0') as i32;
+                i += 1;
+            }
+
+            (i, num)
+        };
+
+        let (mut i, mut product) = read_number(0);
 
         while i < len {
             while i < len && s[i] == b' ' {
@@ -21,7 +42,7 @@ impl Solution {
             let op = s[i];
             i += 1;
 
-            let (num, j) = read_number(s, i);
+            let (j, num) = read_number(i);
             i = j;
 
             match op {
@@ -47,26 +68,6 @@ impl Solution {
 
         sum + product * sign
     }
-}
-
-fn read_number(s: &[u8], mut i: usize) -> (i32, usize) {
-    while i < s.len() && s[i] == b' ' {
-        i += 1;
-    }
-
-    let mut num: i32 = 0;
-
-    while i < s.len() {
-        let c = s[i];
-        if c < b'0' || c > b'9' {
-            break;
-        }
-        num *= 10;
-        num += (s[i] - b'0') as i32;
-        i += 1;
-    }
-
-    (num, i)
 }
 
 #[cfg(test)]
